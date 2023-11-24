9105824c06b9b4d7372c20ab9d2c71007ecd404a
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2023-11-24 16:21:34 +0000

    add css style

--- src/cart.js
+++ src/cart.js
@@ -6,6 +6,7 @@ function addCartContent() {
 
     cartContent.innerHTML = `
         <div class="products_container">
+          <div class="cart-add-products>
             <div class="delete_all">
                 <button type="submit" class="cart-delete_all-button">Delete all
                     <svg class="cart_close_all" width="24" height="24">
@@ -18,14 +19,17 @@ function addCartContent() {
 
                 </div>
             </div>
+            </div>
             <div class="products_order_container">
                 <p class="your_order">Your Order</p>
-                <div>
-                    <p class="cart_total">Total</p>
-                    <div class="cart_total_sum">
-                        <p class="cart_sum">Sum:</p>
-                        <p class="cart_sum_number">$0</p>
+                <div class="total-count">
+                  <div class="total-count-div">
+                    <p class="cart-total">Total</p>
+                    <div class="cart-total-sum">
+                        <p class="cart-sum">Sum:</p>
+                        <p class="cart-sum-number">$0</p>
                     </div>
+                  </div>
                 </div>
                 <form class="order-form">
                      <input type="text" value="Mail: " placeholder="enter your email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
@@ -78,19 +82,24 @@ calculateTotalPrice();
     cartItems.push(product);
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
     const productHTML = `
-      <div>
-        <img src="${product.imageUrl}" alt="Product Image" class="product-image">
-        <h2 class="product-name">${product.name}</h2>
-        <p class="product-category">${product.category}</p>
-        <p class="product-size">${product.size}</p>
-        <p class="product-price">${product.price}</p>
-        
-        <button class="cart-delete-button" onclick="deleteProduct()">X
-             <svg class="cart-delete-icon" width="18" height="18">
-                 <use href="../img/icons.svg#iicon-ion_close-sharp"></use>
-             </svg>
-        </button>
+      
+       <div class="add-img">
+           <img src="${product.imageUrl}" alt="Product Image" class="product-image">
       </div>
+      <div class="add-img-info">
+         <button class="cart-delete-button" onclick="deleteProduct()">X
+           <svg class="cart-delete-icon" width="18" height="18">
+             <use href="../img/icons.svg#iicon-ion_close-sharp"></use>
+           </svg>
+         </button>
+        <h2 class="product-name">${product.name}</h2>
+        <div class="product-category-size">
+          <p class="product-category">Category: ${product.category}</p>
+          <p class="product-size">Size: ${product.size}</p>
+        </div>
+        <p class="product-price">${product.price}</p
+        </div>
+     
     `;
     
     const cartProductsList = document.querySelector('.cart_products_list');
