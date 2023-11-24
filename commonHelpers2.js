415c101eedc5befb14e53869804432111bf88018
andreasrukom19 <rukom@ukr.net>
2023-11-24 20:47:38 +0000

    Deploying to gh-pages from @ andreasrukom19/project-code_busters09@21fd9e30a37b22f413c30d1973bfd4a719779b99 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,44 +1,44 @@
-/* empty css                      */import{a}from"./assets/vendor-a61d8330.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const i of s)if(i.type==="childList")for(const c of i.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function e(s){const i={};return s.integrity&&(i.integrity=s.integrity),s.referrerpolicy&&(i.referrerPolicy=s.referrerpolicy),s.crossorigin==="use-credentials"?i.credentials="include":s.crossorigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function o(s){if(s.ep)return;s.ep=!0;const i=e(s);fetch(s.href,i)}})();class g{constructor(){this.URL="https://food-boutique.b.goit.study/api",this.currentPage=1,this.perPage=9,this.searchQuerry="",this.category=""}getFoodList(){return a.get(`${this.URL}/products?page=${this.currentPage}&limit=${this.perPage}&keyword=${this.searchQuerry}&category=${this.category}`).then(t=>t.data)}findProductById(t){return a.get(`${this.URL}/products/${t}`).then(e=>e.data)}getPopular(){return a.get(`${this.URL}/products/popular?limit=${this.perPage}`).then(t=>t.data)}getDiscount(){return a.get(`${this.URL}/products/discount`).then(t=>t.data)}getCategories(){return a.get(`${this.URL}/products/categories`).then(t=>t.data)}resetPageCounter(){this.currentPage=1}incrementPage(){this.currentPage+=1}subscribe(t){a.post(`${this.URL}/subscription`,{email:t})}order(t,e){a.post(`${this.URL}/orders`,{email:t,products:e})}}const m=document.querySelector(".filter-box__list"),f=document.querySelector(".popular-products__list"),h=document.querySelector(".discount-products__list"),_=document.querySelector(".popular-products__list-responsive"),$=document.querySelector(".discount-products__list-responsive"),u=new g;document.addEventListener("click",function(r){if(r.target.classList.contains("cart-img-products")){const t=r.target,e="../img/checked.svg";t.src=e}});u.getFoodList().then(r=>{m.innerHTML=v(r.results)}).catch(r=>{});u.getPopular().then(r=>{f.innerHTML=p(r),_.innerHTML=p(r)}).catch(r=>{});u.getDiscount().then(r=>{h.innerHTML=l(r),$.innerHTML=l(r)}).catch(r=>{});function v(r){return r.map(({_id:t,name:e,img:o,category:s,price:i,popularity:c,size:n,is10PercentOff:d})=>`<li class="product-card data-id="${t}">
-       ${d?'<img class="discount-icon-products" src="../../assets/discount.svg" alt="Discount" />':""}
-      <div class="img-container"><a href="${o}"><img class="product-card__img" src="${o}" alt="${e}" loading="lazy" /></a>
+/* empty css                      */import{a}from"./assets/vendor-a61d8330.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))o(r);new MutationObserver(r=>{for(const s of r)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function i(r){const s={};return r.integrity&&(s.integrity=r.integrity),r.referrerpolicy&&(s.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?s.credentials="include":r.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(r){if(r.ep)return;r.ep=!0;const s=i(r);fetch(r.href,s)}})();class f{constructor(){this.URL="https://food-boutique.b.goit.study/api",this.currentPage=1,this.perPage=6,this.searchQuerry="",this.category=""}getFoodList(){return window.innerWidth>=768&&window.innerWidth<1440?this.perPage=8:window.innerWidth>=1440&&(this.perPage=9),a.get(`${this.URL}/products?page=${this.currentPage}&limit=${this.perPage}&keyword=${this.searchQuerry}&category=${this.category}`).then(e=>e.data)}findProductById(e){return a.get(`${this.URL}/products/${e}`).then(i=>i.data)}getPopular(){return a.get(`${this.URL}/products/popular`).then(e=>e.data)}getDiscount(){return a.get(`${this.URL}/products/discount`).then(e=>e.data)}getCategories(){return a.get(`${this.URL}/products/categories`).then(e=>e.data)}resetPageCounter(){this.currentPage=1}incrementPage(){this.currentPage+=1}subscribe(e){a.post(`${this.URL}/subscription`,{email:e})}order(e,i){a.post(`${this.URL}/orders`,{email:e,products:i})}}const h=document.querySelector(".filter-box__list"),m=document.querySelector(".popular-products__list"),_=document.querySelector(".discount-products__list"),$=document.querySelector(".popular-products__list-responsive"),v=document.querySelector(".discount-products__list-responsive"),c=new f;document.addEventListener("click",function(t){if(t.target.classList.contains("cart-img-products")){const e=t.target,i="../img/checked.svg";e.src=i}});p();y();L();function p(){c.getFoodList().then(t=>{h.innerHTML=P(t.results)}).catch(t=>{})}function y(){c.getPopular().then(t=>{m.innerHTML=d(t),$.innerHTML=d(t)}).catch(t=>{})}function L(){c.getDiscount().then(t=>{_.innerHTML=l(t),v.innerHTML=l(t)}).catch(t=>{})}function P(t){return t.map(({_id:e,name:i,img:o,category:r,price:s,popularity:n,size:u,is10PercentOff:g})=>`<li class="product-card data-id="${e}">
+       ${g?'<img class="discount-icon-products" src="../../assets/discount.svg" alt="Discount" />':""}
+      <div class="img-container"><a href="${o}"><img class="product-card__img" src="${o}" alt="${i}" loading="lazy" /></a>
       </div>
       <div class="info">      
-        <h2 class="info__title">${e}</h2>
+        <h2 class="info__title">${i}</h2>
         <div class="info-wrapper">
-        <p class="info-wrapper__product">Category:<span>${s}</span></p>
-        <p class="info-wrapper__product">Size:<span>${n}</span></p>
-        <p class="info-wrapper__product">Popularity:<span>${c}</span></p>
+        <p class="info-wrapper__product">Category:<span>${r}</span></p>
+        <p class="info-wrapper__product">Size:<span>${u}</span></p>
+        <p class="info-wrapper__product">Popularity:<span>${n}</span></p>
         </div>
         <div class="info-wrapper__price-container" >
-        <p class="info__price">$${i}</p>       
+        <p class="info__price">$${s}</p>       
           <img class="cart-img-products" src="/project-code_busters09/img/cart.svg" alt="cart" />
         </div>     
       </div>
-    </li>`).join("")}function p(r){return r.map(({_id:t,name:e,img:o,category:s,popularity:i,size:c,is10PercentOff:n})=>`      
-      <li class="popular-item" data-id="${t}">
-      ${n?'<img class="discount-icon-popular" src="./img/discount.svg" alt="Discount" />  <img class="popular-cart-img-down" src="./img/cartLight.svg" alt="cart" />':'<img class="popular-cart-img" src="./img/cartLight.svg" alt="cart" />'}
+    </li>`).join("")}function d(t){return t.map(({_id:e,name:i,img:o,category:r,popularity:s,size:n,is10PercentOff:u})=>`      
+      <li class="popular-item" data-id="${e}">
+      ${u?'<img class="discount-icon-popular" src="./img/discount.svg" alt="Discount" />  <img class="popular-cart-img-down" src="./img/cartLight.svg" alt="cart" />':'<img class="popular-cart-img" src="./img/cartLight.svg" alt="cart" />'}
        
-      <div class="popular-img-container"><a href="${o}"><img class="popular-item__img" src="${o}" alt="${e}" loading="lazy" /></a>
+      <div class="popular-img-container"><a href="${o}"><img class="popular-item__img" src="${o}" alt="${i}" loading="lazy" /></a>
       </div>
       <div class="popular-info">
-        <h3 class="popular-info__title">${e}</h3>
+        <h3 class="popular-info__title">${i}</h3>
         <div class="info-wrapper">
-        <p class="info-wrapper__product">Category:<span>${s}</span></p>
-        <p class="info-wrapper__product">Size:<span>${c}</span></p>
-        <p class="info-wrapper__product">Popularity:<span>${i}</span></p>
+        <p class="info-wrapper__product">Category:<span>${r}</span></p>
+        <p class="info-wrapper__product">Size:<span>${n}</span></p>
+        <p class="info-wrapper__product">Popularity:<span>${s}</span></p>
         </div>             
       </div>
-    </li>`).slice(0,5).join("")}function l(r){return r.map(({_id:t,name:e,img:o,price:s})=>`      
-      <li class="discount-item" data-id="${t}">
+    </li>`).slice(0,5).join("")}function l(t){return t.map(({_id:e,name:i,img:o,price:r})=>`      
+      <li class="discount-item" data-id="${e}">
        <img class="discount-cheap" src="./img/discount.svg" alt="cart" />  
-      <div class="discount-img-container"><a href="${o}"><img class="discount-item__img" src="${o}" alt="${e}" loading="lazy" /></a>
+      <div class="discount-img-container"><a href="${o}"><img class="discount-item__img" src="${o}" alt="${i}" loading="lazy" /></a>
       </div>
       <div class="discount-info">
-        <h3 class="discount-info__title">${e}</h3>
+        <h3 class="discount-info__title">${i}</h3>
         <div class="discount-img-wrapper">
-        <p class="discount-info__price">$${s}</p>
+        <p class="discount-info__price">$${r}</p>
          <img class="cart-img-products" src="./img/cart.svg" alt="cart" />      
         </div>        
       </div>
-    </li>`).slice(0,2).join("")}
+    </li>`).slice(0,2).join("")}window.addEventListener("resize",w);function w(){const t=window.innerWidth;t>=768&&t<1440&&c.perPage!==8?(c.perPage=8,p()):t>=1440&&c.perPage!==9?(c.perPage=9,p()):t<768&&c.perPage!==6&&(c.perPage=6,p())}
 //# sourceMappingURL=commonHelpers2.js.map
