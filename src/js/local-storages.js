dbdd1ce8e65e31da07b46455c6b310df418b12a7
mariavsl518 <mariavsl518@gmail.com>
2023-11-24 09:10:49 +0100

    Update local-storages.js

--- src/js/local-storages.js
+++ src/js/local-storages.js
@@ -7,26 +7,42 @@ const foodService = new FoodService();
 
 export class LocalStorage {
 
-    constructor() { 
+    constructor() {
 
     }
 
-    saveAllToLocalStorage() { 
-        if (!localStorage.getItem('products')) { 
+    saveAllToLocalStorage() {
+        if (!localStorage.getItem('products')) {
             return foodService.getFoodList()
-            .then(resp => {
-            localStorage.setItem('products', JSON.stringify(resp.results))
-        })
+                .then(resp => {
+                    localStorage.setItem('products', JSON.stringify(resp.results))
+                })
         }
-}
+    }
+
+    addToCart(id) {
+        // test 
+        const products = JSON.parse(localStorage.getItem('products'));
+        // test
+
+        if (!localStorage.getItem('cart'))
+        { 
+            localStorage.setItem('cart')
+        }
+        const item = products.find(item => item._id === id);
+        const cart = JSON.parse(localStorage.getItem('cart'));
+        cart.push(item)
+        }
     
-    getFromLocalStorage(item) { 
-        return localStorage.getItem(`${item}`).JSON.parse()
+    getFromLocalStorage(item) {
+        return JSON.parse(localStorage.getItem(`${item}`));
     }
 
-    addToCart() { 
-        
-    }
 
 }
-localStorage.getFromLocalStorage()
+
+const storage = new LocalStorage;
+storage.addToCart('640c2dd963a319ea671e383b');
+storage.addToCart('640c2dd963a319ea671e3660');
+
+
