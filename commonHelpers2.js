50fe8248453519aaea5364457f61acd2b976ddf5
IgorAvd <142496207+IgorAvd@users.noreply.github.com>
2023-11-24 09:08:52 +0000

    Deploying to gh-pages from @ andreasrukom19/project-code_busters09@7fce15813485f8bad9f46dff42508b68fbe191a9 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,34 +1,42 @@
-/* empty css                      */import{a}from"./assets/vendor-a365bedd.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=s(e);fetch(e.href,o)}})();class p{constructor(){this.URL="https://food-boutique.b.goit.study/api",this.currentPage=1,this.perPage=9,this.searchQuerry="",this.category=""}getFoodList(){return a.get(`${this.URL}/products?page=${this.currentPage}&limit=${this.perPage}&keyword=${this.searchQuerry}&category=${this.category}`).then(t=>t.data)}findProductById(t){return a.get(`${this.URL}/products/${t}`).then(s=>s.data)}getPopular(){return a.get(`${this.URL}/products/popular?limit=${this.perPage}`).then(t=>t.data)}getDiscount(){return a.get(`${this.URL}/products/discount`).then(t=>t.data)}getCategories(){return a.get(`${this.URL}/products/categories`).then(t=>t.data)}resetPageCounter(){this.currentPage=1}incrementPage(){this.currentPage+=1}subscribe(t){a.post(`${this.URL}/subscription`,{email:t})}order(t,s){a.post(`${this.URL}/orders`,{email:t,products:s})}}const u=document.querySelector(".filter-box__list"),l=document.querySelector(".popular-products__list"),d=document.querySelector(".discount-products__list"),c=new p;c.getFoodList().then(r=>{u.innerHTML=f(r.results)}).catch(r=>{});c.getPopular().then(r=>{l.innerHTML=g(r)}).catch(r=>{});c.getDiscount().then(r=>{console.log("data",r),d.innerHTML=h(r)}).catch(r=>{});function f(r){return r.map(({name:t,img:s,category:i,price:e,popularity:o,size:n})=>`<li class="product-card">
-      <div class="img-container"><a href="${s}"><img class="product-card__img" src="${s}" alt="${t}" loading="lazy" /></a>
+/* empty css                      */import{a as c}from"./assets/vendor-a365bedd.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))o(r);new MutationObserver(r=>{for(const s of r)if(s.type==="childList")for(const a of s.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function i(r){const s={};return r.integrity&&(s.integrity=r.integrity),r.referrerpolicy&&(s.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?s.credentials="include":r.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(r){if(r.ep)return;r.ep=!0;const s=i(r);fetch(r.href,s)}})();class l{constructor(){this.URL="https://food-boutique.b.goit.study/api",this.currentPage=1,this.perPage=9,this.searchQuerry="",this.category=""}getFoodList(){return c.get(`${this.URL}/products?page=${this.currentPage}&limit=${this.perPage}&keyword=${this.searchQuerry}&category=${this.category}`).then(t=>t.data)}findProductById(t){return c.get(`${this.URL}/products/${t}`).then(i=>i.data)}getPopular(){return c.get(`${this.URL}/products/popular?limit=${this.perPage}`).then(t=>t.data)}getDiscount(){return c.get(`${this.URL}/products/discount`).then(t=>t.data)}getCategories(){return c.get(`${this.URL}/products/categories`).then(t=>t.data)}resetPageCounter(){this.currentPage=1}incrementPage(){this.currentPage+=1}subscribe(t){c.post(`${this.URL}/subscription`,{email:t})}order(t,i){c.post(`${this.URL}/orders`,{email:t,products:i})}}const d=document.querySelector(".filter-box__list"),g=document.querySelector(".popular-products__list"),f=document.querySelector(".discount-products__list"),h=document.querySelector(".popular-products__list-responsive"),m=document.querySelector(".discount-products__list-responsive"),n=new l;n.getFoodList().then(e=>{d.innerHTML=_(e.results)}).catch(e=>{});n.getPopular().then(e=>{g.innerHTML=p(e),h.innerHTML=p(e)}).catch(e=>{});n.getDiscount().then(e=>{f.innerHTML=u(e),m.innerHTML=u(e)}).catch(e=>{});function _(e){return e.map(({name:t,img:i,category:o,price:r,popularity:s,size:a})=>`<li class="product-card">
+      <div class="img-container"><a href="${i}"><img class="product-card__img" src="${i}" alt="${t}" loading="lazy" /></a>
       </div>
       <div class="info">
         <h2 class="info__title">${t}</h2>
         <div class="info-wrapper">
-        <p class="info-wrapper__product">Category:<span>${i}</span></p>
-        <p class="info-wrapper__product">Size:<span>${n}</span></p>
-        <p class="info-wrapper__product">Popularity:<span>${o}</span></p>
+        <p class="info-wrapper__product">Category:<span>${o}</span></p>
+        <p class="info-wrapper__product">Size:<span>${a}</span></p>
+        <p class="info-wrapper__product">Popularity:<span>${s}</span></p>
+        </div>
+        <div class="info-wrapper__price-container" >
+        <p class="info__price">$${r}</p>
+        <img class="cart-img" src="../img/cart.svg" alt="cart" />
         </div>     
-        <p class="info__price">$${e}</p>
       </div>
-    </li>`).join("")}function g(r){return r.map(({name:t,img:s,category:i,popularity:e,size:o})=>`      
+    </li>`).join("")}function p(e){return e.map(({name:t,img:i,category:o,popularity:r,size:s})=>`      
       <li class="popular-item">
-      <div class="popular-img-container"><a href="${s}"><img class="popular-item__img" src="${s}" alt="${t}" loading="lazy" /></a>
+       <img class="popular-cart-img" src="../img/cartLight.svg" alt="cart" />  
+      <div class="popular-img-container"><a href="${i}"><img class="popular-item__img" src="${i}" alt="${t}" loading="lazy" /></a>
       </div>
       <div class="popular-info">
         <h3 class="popular-info__title">${t}</h3>
         <div class="info-wrapper">
-        <p class="info-wrapper__product">Category:<span>${i}</span></p>
-        <p class="info-wrapper__product">Size:<span>${o}</span></p>
-        <p class="info-wrapper__product">Popularity:<span>${e}</span></p>
+        <p class="info-wrapper__product">Category:<span>${o}</span></p>
+        <p class="info-wrapper__product">Size:<span>${s}</span></p>
+        <p class="info-wrapper__product">Popularity:<span>${r}</span></p>
         </div>             
       </div>
-    </li>`).slice(0,5).join("")}function h(r){return r.map(({name:t,img:s,price:i})=>`      
+    </li>`).slice(0,5).join("")}function u(e){return e.map(({name:t,img:i,price:o})=>`      
       <li class="discount-item">
-      <div class="discount-img-container"><a href="${s}"><img class="discount-item__img" src="${s}" alt="${t}" loading="lazy" /></a>
+       <img class="discount-cheap" src="../img/discount.svg" alt="cart" />  
+      <div class="discount-img-container"><a href="${i}"><img class="discount-item__img" src="${i}" alt="${t}" loading="lazy" /></a>
       </div>
       <div class="discount-info">
-        <h3 class="discount-info__title">${t}</h3>        
-        <p class="discount-info__price">$${i}</p>      
+        <h3 class="discount-info__title">${t}</h3>
+        <div class="discount-img-wrapper">
+        <p class="discount-info__price">$${o}</p>
+         <img class="cart-img" src="../img/cart.svg" alt="cart" />      
+        </div>        
       </div>
-    </li>`).slice(0,2).join("")}a.defaults.baseURL="https://food-boutique.b.goit.study/api";new p;localStorage.getFromLocalStorage();
+    </li>`).slice(0,2).join("")}c.defaults.baseURL="https://food-boutique.b.goit.study/api";new l;localStorage.getFromLocalStorage();
 //# sourceMappingURL=commonHelpers2.js.map
