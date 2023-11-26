08ab258f16e6bc732f9cc3eec20628e8475ad430
Андрій <rukom@ukr.net>
2023-11-26 22:53:39 +0200

    Update cart.js

--- src/cart.js
+++ src/cart.js
@@ -1,5 +1,5 @@
 
-import  {LocalStorage}  from './local-storages';
+import  { storage }  from './js/mainSection';
 
 
 const deleteBtn = document.querySelector(".cart-delete_all-button");
@@ -7,21 +7,21 @@ const deleteAllBtn = document.querySelector(".cart_close_all");
 const cartContent = document.getElementById("cart-content");
 const cartProductsList = document.querySelector(".cart_products_list");
 const cartProductsContainer = document.querySelector('.cart_products_container');
+console.log(cartContent);
 
-const storage = new LocalStorage();
-deleteBtn.addEventListener('click', storage.removeFromCart());
+// const storage = new LocalStorage();
+// deleteBtn.addEventListener('click', storage.removeFromCart());
 
-deleteAllBtn.addEventListener('click', function() {
-  storage.removeFromCart();
-  cartContent.innerHTML = createCartMarkupDefault();;
-});
-
+// deleteAllBtn.addEventListener('click', function() {
+//   storage.removeFromCart();
+//   cartContent.innerHTML = createCartMarkupDefault();;
+// });
 
 
 //функція загальної кількості продуктів, доданих у кошик
 
 function totalQuantity() {
-    const items = LocalStorage.getFromStorage();
+    const items = storage.getFromStorage();
     const totalCount = items.reduce((total, currentItem) => (
     total + currentItem
 ), 0)
@@ -31,8 +31,10 @@ function totalQuantity() {
 // Функція для розрахунку загальної вартості продуктів
 
 function calculateTotalPrice() {
-  storage.getFromStorage();
-    const cartSumNumber = document.querySelector(".cart_sum_number");
+  const products = storage.getFromStorage('cart');
+  console.log(products);
+  const cartSumNumber = document.querySelector(".cart-sum-number");
+  console.log(cartSumNumber);
     let totalPrice = 0;
     for (let i = 0; i < products.length; i++) {
         totalPrice += products[i].price;
@@ -40,24 +42,24 @@ function calculateTotalPrice() {
     cartSumNumber.textContent = `${totalPrice}`;
 }
 
-calculateTotalPrice();
+// calculateTotalPrice();
 
 
 
-
- function checkLocalStorage() {
+function checkLocalStorage() {
   
-    const cart = storage.getFromStorage("item"); 
-    if (cart !== 0)  {
-      cartContent.innerHTML = createCartMarkup(cart);
-      cartProductsList.innerHTML = createCartMarkupProducts(cart.products);
+  const cart = storage.getFromStorage('cart');
+  console.log(cart);
+    if (!cart)  {
+      cartContent.innerHTML = createCartMarkup();
+      cartProductsList.innerHTML = createCartMarkupProducts(cart);
     } else {
       cartContent.innerHTML = createCartMarkupDefault();
     }
   
 }
 
-
+checkLocalStorage();
 
 function createCartMarkup () {
   return `<div class="products_container">
@@ -100,8 +102,8 @@ function createCartMarkup () {
         </label>
       </div>
       <div class="cart-btn">
-      <button type="submit" class="cart-checkout-button">Checkout</button>
-    </div>
+        <button type="submit" class="cart-checkout-button">Checkout</button>
+      </div>
     </form>
     
   </div>`
@@ -118,7 +120,7 @@ function createCartMarkupProducts(products) {
           <div class="add-img-info">
               <button class="cart-delete-button">
                 <svg class="cart_close_all" width="18" height="18">
-                   <use xlink:href="../img/icons.svg#icon-ion_close-sharp"></use>
+                    <use xlink:href="../img/icons.svg#icon-ion_close-sharp"></use>
                 </svg>
               </button>
             <h2 class="product-name">${name}</h2>
@@ -163,22 +165,19 @@ function createCartMarkupDefault() {
 };
 
 //scroll
-if (localStorage.getFromStorage('cart_products_list')) {
-const products = JSON.parse(localStorage.getFromStorage('cart_products_list'));
-if (products.length > 3) {
-
-
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {    
-      if (entry.isIntersecting) {
-          cartProductsContainer.classList.add('scroll');
-      } else {
-          cartProductsContainer.classList.remove('scroll');
-        }
-});
-});
-observer.observe(cartProductsList);
-}
-}
+// if (storage.getFromStorage('cart')) {
+//   const products = storage.getFromStorage('cart');
+// if (products.length > 3) {
+//   const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {    
+//       if (entry.isIntersecting) {
+//           cartProductsContainer.classList.add('scroll');
+//       } else {
+//           cartProductsContainer.classList.remove('scroll');
+//         }
+//   });
+// });
+//   observer.observe(cartProductsList);
+// }
+// }
 
