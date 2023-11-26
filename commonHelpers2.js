393415d3a90b99b660dd9d92a616ad12ff88b5bb
andreasrukom19 <rukom@ukr.net>
2023-11-26 13:14:25 +0000

    Deploying to gh-pages from @ andreasrukom19/project-code_busters09@43a4eb5f0b211604fd323aa440cabff9ce408f19 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,35 +1,35 @@
-/* empty css                      */import{a as c}from"./assets/vendor-a61d8330.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))i(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function o(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerpolicy&&(r.referrerPolicy=s.referrerpolicy),s.crossorigin==="use-credentials"?r.credentials="include":s.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(s){if(s.ep)return;s.ep=!0;const r=o(s);fetch(s.href,r)}})();class E{constructor(){}saveInStorage(e,o){localStorage.setItem(e,JSON.stringify(o))}saveAllToLocalStorage(e){localStorage.setItem("products",JSON.stringify(e))}saveCategories(){if(!localStorage.getItem("categories"))return n.getCategories().then(e=>{console.log(e),localStorage.setItem("categories",JSON.stringify(e))})}defaultApiOptions(){const e={keyword:null,category:null,page:1,limit:6};localStorage.setItem("options",JSON.stringify(e))}getApiOptions(){return JSON.parse(localStorage.getItem("options"))}setApiOptions(e,o){JSON.parse(localStorage.getItem("options")),options[e]=o}addToCart(e){const i=JSON.parse(localStorage.getItem("products")).find(s=>s._id===e);if(console.log(i),localStorage.getItem("cart")){const s=JSON.parse(localStorage.getItem("cart"));return localStorage.setItem("cart",JSON.stringify(s))}else return localStorage.setItem("cart",JSON.stringify(i))}getFromStorage(e){return JSON.parse(localStorage.getItem(`${e}`))}removeFromCart(e){localStorage.getItem("products"),JSON.parce(localStorage.getItem("cart"))}clearCart(){}saveOptionsToFoodService(e){n.perPage=e.limit,n.currentPage=e.page,n.category=e.category,n.searchQuerry=e.keyword}}class C{constructor(){this.URL="https://food-boutique.b.goit.study/api",this.currentPage=1,this.perPage=6,this.searchQuerry="",this.category=""}getBasicFoodList(){return window.innerWidth>=768&&window.innerWidth<1440?this.perPage=8:window.innerWidth>=1440&&(this.perPage=9),c.get(`${this.URL}/products?limit=${this.perPage}`).then(e=>e.data)}getFoodListWithOptions(){return window.innerWidth>=768&&window.innerWidth<1440?this.perPage=8:window.innerWidth>=1440&&(this.perPage=9),c.get(`${this.URL}/products?page=${this.currentPage}&limit=${this.perPage}&keyword=${this.searchQuerry}&category=${this.category}`).then(e=>e.data)}findProductById(e){return c.get(`${this.URL}/products/${e}`).then(o=>o.data)}getPopular(){return c.get(`${this.URL}/products/popular`).then(e=>e.data)}getDiscount(){return c.get(`${this.URL}/products/discount`).then(e=>e.data)}getCategories(){return c.get(`${this.URL}/products/categories`).then(e=>e.data)}resetPageCounter(){this.currentPage=1}incrementPage(){this.currentPage+=1}resetSearchQuerry(){this.searchQuerry=""}resetCategory(){this.category=""}subscribe(e){c.post(`${this.URL}/subscription`,{email:e})}order(e,o){c.post(`${this.URL}/orders`,{email:e,products:o})}}const O="/project-code_busters09/assets/cart-a9135e8e.svg",y="/project-code_busters09/assets/cartLight-0b4d3d6f.svg",v="/project-code_busters09/assets/discount-b955391f.svg",P=document.querySelector(".filter-box__list"),M=document.querySelector(".popular-products__list"),T=document.querySelector(".discount-products__list"),N=document.querySelector(".popular-products__list-responsive"),k=document.querySelector(".discount-products__list-responsive"),p=new E,n=new C;document.addEventListener("click",function(t){if(t.target.classList.contains("cart-img-products")){const e=t.target,o="../img/checked.svg";e.src=o}});L();A();H();function L(){n.getBasicFoodList().then(t=>{console.log(t),P.innerHTML=b(t.results),p.saveAllToLocalStorage(t.results),p.defaultApiOptions()}).catch(t=>{})}function m(){const t=p.getApiOptions();n.getFoodListWithOptions(t).then(e=>{P.innerHTML=b(e.results),p.saveAllToLocalStorage(e.results)}).catch(e=>{})}function A(){n.getPopular().then(t=>{M.innerHTML=S(t),N.innerHTML=S(t)}).catch(t=>{})}function H(){n.getDiscount().then(t=>{T.innerHTML=$(t),k.innerHTML=$(t)}).catch(t=>{})}function b(t){return t.map(({_id:e,name:o,img:i,category:s,price:r,popularity:a,size:h,is10PercentOff:g})=>{const l=document.createElement("img");l.src=O,l.classList.add("main-cart-icon");const d=document.createElement("img");d.src=v,d.classList.add("discount-icon-products");const f=g?`${d.outerHTML}`:"";return`<li class="product-card" data-id=${e}>
-        ${f}
+/* empty css                      */import{a}from"./assets/vendor-a61d8330.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))i(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const c of r.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function o(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerpolicy&&(r.referrerPolicy=s.referrerpolicy),s.crossorigin==="use-credentials"?r.credentials="include":s.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(s){if(s.ep)return;s.ep=!0;const r=o(s);fetch(s.href,r)}})();class T{constructor(){}saveInStorage(e,o){localStorage.setItem(e,JSON.stringify(o))}saveAllToLocalStorage(e){localStorage.setItem("products",JSON.stringify(e))}saveCategories(){if(!localStorage.getItem("categories"))return n.getCategories().then(e=>{console.log(e),localStorage.setItem("categories",JSON.stringify(e))})}defaultApiOptions(){const e={keyword:null,category:null,page:1,limit:6};localStorage.setItem("options",JSON.stringify(e))}getApiOptions(){return JSON.parse(localStorage.getItem("options"))}setApiOptions(e,o){JSON.parse(localStorage.getItem("options")),options[e]=o}addToCart(e){const i=JSON.parse(localStorage.getItem("products")).find(s=>s._id===e);if(console.log(i),localStorage.getItem("cart")){const s=JSON.parse(localStorage.getItem("cart"));return localStorage.setItem("cart",JSON.stringify(s))}else return localStorage.setItem("cart",JSON.stringify(i))}getFromStorage(e){return JSON.parse(localStorage.getItem(`${e}`))}removeFromCart(e){localStorage.getItem("products"),JSON.parce(localStorage.getItem("cart"))}clearCart(){}saveOptionsToFoodService(e){n.perPage=e.limit,n.currentPage=e.page,n.category=e.category,n.searchQuerry=e.keyword}}class k{constructor(){this.URL="https://food-boutique.b.goit.study/api",this.currentPage=1,this.perPage=6,this.searchQuerry="",this.category=""}getBasicFoodList(){return window.innerWidth>=768&&window.innerWidth<1440?this.perPage=8:window.innerWidth>=1440&&(this.perPage=9),a.get(`${this.URL}/products?limit=${this.perPage}`).then(e=>e.data)}getFoodListWithOptions(){return window.innerWidth>=768&&window.innerWidth<1440?this.perPage=8:window.innerWidth>=1440&&(this.perPage=9),a.get(`${this.URL}/products?page=${this.currentPage}&limit=${this.perPage}&keyword=${this.searchQuerry}&category=${this.category}`).then(e=>e.data)}findProductById(e){return a.get(`${this.URL}/products/${e}`).then(o=>o.data)}getPopular(){return a.get(`${this.URL}/products/popular`).then(e=>e.data)}getDiscount(){return a.get(`${this.URL}/products/discount`).then(e=>e.data)}getCategories(){return a.get(`${this.URL}/products/categories`).then(e=>e.data)}resetPageCounter(){this.currentPage=1}incrementPage(){this.currentPage+=1}resetSearchQuerry(){this.searchQuerry=""}resetCategory(){this.category=""}subscribe(e){a.post(`${this.URL}/subscription`,{email:e})}order(e,o){a.post(`${this.URL}/orders`,{email:e,products:o})}}const O="/project-code_busters09/assets/cart-a9135e8e.svg",S="/project-code_busters09/assets/cartLight-0b4d3d6f.svg",y="/project-code_busters09/assets/discount-b955391f.svg",b=document.querySelector(".filter-box__list"),N=document.querySelector(".popular-products__list"),A=document.querySelector(".discount-products__list"),H=document.querySelector(".popular-products__list-responsive"),R=document.querySelector(".discount-products__list-responsive"),p=new T,n=new k;document.addEventListener("click",function(t){if(t.target.classList.contains("cart-img-products")){const e=t.target,o="../img/checked.svg";e.src=o}});v();q();J();function v(){n.getBasicFoodList().then(t=>{console.log(t),b.innerHTML=P(t.results),p.saveAllToLocalStorage(t.results),p.defaultApiOptions()}).catch(t=>{})}function m(){const t=p.getApiOptions();n.getFoodListWithOptions(t).then(e=>{b.innerHTML=P(e.results),p.saveAllToLocalStorage(e.results)}).catch(e=>{})}function q(){n.getPopular().then(t=>{N.innerHTML=$(t),H.innerHTML=$(t)}).catch(t=>{})}function J(){n.getDiscount().then(t=>{A.innerHTML=w(t),R.innerHTML=w(t)}).catch(t=>{})}function P(t){return t.map(({_id:e,name:o,img:i,category:s,price:r,popularity:c,size:f,is10PercentOff:g})=>{const l=document.createElement("img");l.src=O,l.classList.add("main-cart-icon");const d=document.createElement("img");d.src=y,d.classList.add("discount-icon-products");const h=g?`${d.outerHTML}`:"";return`<li class="product-card" data-id=${e}>
+        ${h}
           <div class="img-container"><a href="${i}"><img class="product-card__img" src="${i}" alt="${o}" loading="lazy" /></a>
           </div>
           <div class="info">      
             <h2 class="info__title">${o}</h2>
             <div class="info-wrapper">
               <p class="info-wrapper__product">Category:<span>${s}</span></p>
-              <p class="info-wrapper__product">Size:<span>${h}</span></p>
-              <p class="info-wrapper__product">Popularity:<span>${a}</span></p>
+              <p class="info-wrapper__product">Size:<span>${f}</span></p>
+              <p class="info-wrapper__product">Popularity:<span>${c}</span></p>
             </div>
             <div class="info-wrapper__price-container" >
               <p class="info__price">$${r}</p> 
                ${l.outerHTML}
             </div>     
           </div>
-        </li>`}).join("")}function S(t){return t.map(({_id:e,name:o,img:i,category:s,popularity:r,size:a,is10PercentOff:h})=>{const g=document.createElement("img");g.src=y,g.classList.add("popular-cart-img");const l=document.createElement("img");l.src=y,l.classList.add("popular-cart-img-down");const d=document.createElement("img");d.src=v,d.classList.add("discount-icon-popular");const f=h?`${d.outerHTML}${l.outerHTML}`:`${g.outerHTML}`;return`      
+        </li>`}).join("")}function $(t){return t.map(({_id:e,name:o,img:i,category:s,popularity:r,size:c,is10PercentOff:f})=>{const g=document.createElement("img");g.src=S,g.classList.add("popular-cart-img");const l=document.createElement("img");l.src=S,l.classList.add("popular-cart-img-down");const d=document.createElement("img");d.src=y,d.classList.add("discount-icon-popular");const h=f?`${d.outerHTML}${l.outerHTML}`:`${g.outerHTML}`;return`      
         <li class="popular-item" data-id="${e}">
-          ${f}
+          ${h}
           <div class="popular-img-container"><img class="popular-item__img" src="${i}" alt="${o}" loading="lazy" /></div>
           <div class="popular-info">
             <h3 class="popular-info__title">${o}</h3>
             <div class="info-wrapper">
               <p class="info-wrapper__product">Category:<span>${s}</span></p>
-              <p class="info-wrapper__product">Size:<span>${a}</span></p>
+              <p class="info-wrapper__product">Size:<span>${c}</span></p>
               <p class="info-wrapper__product">Popularity:<span>${r}</span></p>
             </div>             
           </div>
-        </li>`}).slice(0,5).join("")}function $(t){return t.map(({_id:e,name:o,img:i,price:s})=>{const r=document.createElement("img");r.src=O,r.classList.add("cart-img-products");const a=document.createElement("img");return a.src=v,a.classList.add("discount-cheap"),`      
+        </li>`}).slice(0,5).join("")}function w(t){return t.map(({_id:e,name:o,img:i,price:s})=>{const r=document.createElement("img");r.src=O,r.classList.add("cart-img-products");const c=document.createElement("img");return c.src=y,c.classList.add("discount-cheap"),`      
       <li class="discount-item" data-id="${e}">
    
-       ${a.outerHTML} 
+       ${c.outerHTML} 
       <div class="discount-img-container"><a href="${i}"><img class="discount-item__img" src="${i}" alt="${o}" loading="lazy" /></a>
       </div>
       <div class="discount-info">
@@ -39,29 +39,30 @@
         ${r.outerHTML} 
         </div>        
       </div>
-    </li>`}).slice(0,2).join("")}window.addEventListener("resize",R);function R(){const t=window.innerWidth;t>=768&&t<1440&&n.perPage!==8?(n.perPage=8,m()):t>=1440&&n.perPage!==9?(n.perPage=9,m()):t<768&&n.perPage!==6&&(n.perPage=6,m())}const I=document.querySelector(".submit-form"),u=document.querySelector(".categories"),w=document.querySelector(".filter-box__list");document.addEventListener("DOMContentLoaded",function(){I.elements.search.value="",u.value=""});I.addEventListener("submit",t=>{t.preventDefault();const e=t.target.elements.search.value;n.searchQuerry=e;const o=p.getApiOptions();o.keyword=e||null,localStorage.setItem("options",JSON.stringify(o)),e?m():(n.resetSearchQuerry(),L())});u.addEventListener("change",function(){const t=u.value,e=p.getApiOptions();e.category=t||null,localStorage.setItem("options",JSON.stringify(e)),u.value==="show-all"?(w.innerHTML="",L()):(n.category=t,w.innerHTML="",m())});n.getCategories().then(t=>{const e=document.createElement("option");e.value="",e.textContent="Categories",e.disabled=!0,e.selected=!0;const o=t.map(s=>{const r=document.createElement("option");return r.value=s,r.textContent=s,r}),i=J();o.push(i),u.appendChild(e),u.append(...o)});function J(){const t=document.createElement("option");return t.textContent="Show All",t.value="show-all",t}const q={pagesRibbonEL:document.querySelector(".pag-ribbon")};q.pagesRibbonEL.addEventListener("click",U);function U(t){const e=t.target.closest("button").id;console.log(e)}const _=document.querySelector(".modal"),F=document.querySelector(".main-products");F.addEventListener("click",z);async function z(t){const e=t.target.closest("li");if(e){const o=await n.findProductById(e.dataset.id);console.log(o),_.innerHTML=D(o)}_.style.display="block"}function D(t){return`
-  <div class="backdrop"></div>
-  <div class='modal-content'>
-  <button class='modal-btn-close'>
-    <svg class='close-sharp' width='16' height='16'>
-      <use href='./img/icons.svg#icon-ion_close-sharp'></use>
-    </svg></button>
-  <div class='modal-img-container'>
-    <img src="${t.img}" alt="${t.name}" class="modal-image" /></div>
-  <div class='modal-info'>
-    <h2 class='modal-title'>${t.name}</h2>
-    <div class='modal-info-wrapper'>
-              <p class='modal-product'>Category: <span>${t.category}</span></p>
-              <p class='modal-product'>Size: <span>${t.size}</span></p>
-              <p class='modal-product'>Popularity: <span>${t.popularity}</span></p>
-    </div>
-    <p class='modal-info-desc'>${t.desc}</p>
+    </li>`}).slice(0,2).join("")}window.addEventListener("resize",U);function U(){const t=window.innerWidth;t>=768&&t<1440&&n.perPage!==8?(n.perPage=8,m()):t>=1440&&n.perPage!==9?(n.perPage=9,m()):t<768&&n.perPage!==6&&(n.perPage=6,m())}const E=document.querySelector(".submit-form"),u=document.querySelector(".categories"),_=document.querySelector(".filter-box__list");document.addEventListener("DOMContentLoaded",function(){E.elements.search.value="",u.value=""});E.addEventListener("submit",t=>{t.preventDefault();const e=t.target.elements.search.value;n.searchQuerry=e;const o=p.getApiOptions();o.keyword=e||null,localStorage.setItem("options",JSON.stringify(o)),e?m():(n.resetSearchQuerry(),v())});u.addEventListener("change",function(){const t=u.value,e=p.getApiOptions();e.category=t||null,localStorage.setItem("options",JSON.stringify(e)),u.value==="show-all"?(_.innerHTML="",v()):(n.category=t,_.innerHTML="",m())});n.getCategories().then(t=>{const e=document.createElement("option");e.value="",e.textContent="Categories",e.disabled=!0,e.selected=!0;const o=t.map(s=>{const r=document.createElement("option");return r.value=s,r.textContent=s,r}),i=F();o.push(i),u.appendChild(e),u.append(...o)});function F(){const t=document.createElement("option");return t.textContent="Show All",t.value="show-all",t}const z={pagesRibbonEL:document.querySelector(".pag-ribbon")};z.pagesRibbonEL.addEventListener("click",D);function D(t){const e=t.target.closest("button").id;console.log(e)}const I=document.querySelector(".modal"),W=document.querySelector(".modal-content"),x=document.querySelector(".main-products");x.addEventListener("click",B);async function B(t){if(t.target.classList.contains("main-cart-icon"))return;const e=t.target.closest("li");if(e){const o=await n.findProductById(e.dataset.id);W.innerHTML=Q(o)}I.style.display="block",document.body.classList.add("no-scroll"),document.addEventListener("keydown",C),window.addEventListener("click",M)}function Q(t){return`
+  <button class="modal-btn-close">
+  <svg class="close-sharp" width="16" height="16">
+    <use href="./img/icons.svg#icon-ion_close-sharp"></use>
+  </svg>
+</button>
+<div class="modal-img-container">
+  <img src="${t.img}" alt="${t.name}" class="modal-image" />
+</div>
+<div class="modal-info">
+  <h2 class="modal-title">${t.name}</h2>
+  <div class="modal-info-wrapper">
+    <p class="modal-product">Category: <span>${t.category}</span></p>
+    <p class="modal-product">Size: <span>${t.size}</span></p>
+    <p class="modal-product">Popularity: <span>${t.popularity}</span></p>
   </div>
-    <p class='modal-price'>$${t.price}</p>
-    <button class="add-to-cart-btn">Add to
-    <svg class='modal-cart' width='18' height='18'>
-    <use href='./img/icons.svg#icon-heroicons-solid_shopping-cart'></use>
-    </svg>
-    </button>
-  </div>`}
+  <p class="modal-info-desc">${t.desc}</p>
+</div>
+<p class="modal-price">$${t.price}</p>
+<button class="add-to-cart-btn">
+  Add to
+  <svg class="modal-cart" width="18" height="18">
+    <use href="./img/icons.svg#icon-heroicons-solid_shopping-cart"></use>
+  </svg>
+</button>
+`}function C(t){t.key==="Escape"&&L()}function M(t){(t.target.classList.contains("modal")||t.target.closest(".modal-btn-close"))&&L()}function L(){I.style.display="none",document.removeEventListener("keydown",C),window.removeEventListener("click",M),document.body.classList.remove("no-scroll")}
 //# sourceMappingURL=commonHelpers2.js.map
