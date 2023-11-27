ec6ebfaa55707504454bd8da74e0a5224e57cdfa
Андрій <rukom@ukr.net>
2023-11-27 13:12:38 +0200

    Update cart.js

--- src/cart.js
+++ src/cart.js
@@ -126,7 +126,7 @@ function createCartMarkupProducts(products) {
     <div class="obj-delete">
 <button class="cart-delete-button">
 <svg class="cart_close_all" width="18" height="18">
-<use xlink:href=".${iconsURL}#icon-ion_close-sharp"></use>
+<use xlink:href="${iconsURL}#icon-ion_close-sharp"></use>
 </svg>
 </button>
 </div>
