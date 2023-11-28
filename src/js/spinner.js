bbf1a98931baf1bccc4ae21e618db92b96daff43
anetten <anet.zvekova@gmail.com>
2023-11-28 22:55:58 +0200

    upd spinner.js, css and edit modal css(delete animation)

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,21 +1,13 @@
 const spinnerContainer = document.querySelector('.loader-container');
 
 export function showSpinner() {
-  spinnerContainer.style.display = 'flex';
+  if (spinnerContainer) {
+    spinnerContainer.style.display = 'flex';
+  }
 }
 
 export function hideSpinner() {
-  spinnerContainer.style.display = 'none';
+  if (spinnerContainer) {
+    spinnerContainer.style.display = 'none';
+  }
 }
-
-// function openModal() {
-//   showSpinner();
-//   setTimeout(() => {
-//     hideSpinner();
-//     document.querySelector('modal').style.display = 'block';
-//   }, 3000);
-// }
-
-// function closeModal() {
-//   document.querySelector('modal').style.display = 'none';
-// }
