f01e358d1f1e4c25b8126599343612f4a4586e64
HannaSydoruk <135704265+HannaSydoruk@users.noreply.github.com>
2023-11-30 10:32:27 +0000

    Deploying to gh-pages from @ andreasrukom19/project-code_busters09@7166d644c9ee91dcb0cfb674dd7b4f60b0d3b51b 🚀

--- assets/icons-03779629.js
+++ assets/icons-03779629.js
@@ -40,4 +40,4 @@ import{a as g}from"./vendor-99d50140.js";(function(){const t=document.createElem
         </div>        
       </div>
     </li>`}).slice(0,2).join("")}const X="/project-code_busters09/assets/icons-91d3aeb0.svg";export{b as a,O as c,v as d,S as f,x as h,X as i,C as p,i as s,$ as u};
-//# sourceMappingURL=icons-d533626e.js.map
+//# sourceMappingURL=icons-03779629.js.map
--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,9 @@
-import{s as r,u as g,i as u,f as L}from"./assets/icons-d533626e.js";import"./assets/vendor-99d50140.js";const p="/project-code_busters09/assets/yellow_basket_desctop_1x-min-6400acde.png",$="/project-code_busters09/assets/yellow_basket_desctop_1x-min-6400acde.png",w="/project-code_busters09/assets/yellow_basket_mobile_1x-min-195418a1.png",M="/project-code_busters09/assets/yellow_basket_desctop_2x-min-91220a6c.png",S="/project-code_busters09/assets/yellow_basket_desctop_2x-min-91220a6c.png",q="/project-code_busters09/assets/yellow_basket_mobile_2x-min-566aede4.png",b=document.querySelector(".modal"),E=document.querySelector(".modal-content"),a=document.getElementById("cart-content");a.addEventListener("click",j);document.querySelector(".cart_products_container");const v=document.querySelector(".cart-quentity");let e=r.getFromStorage("cart");function j(t){t.target.closest(".cart-delete-all-button")&&(r.clearCart(),g(),v.textContent="Cart(0)",a.innerHTML=l())}function y(){if(e){const t=e.length;v.textContent=`Cart (${t})`}}y();function f(){let t=0;return e&&e.forEach(c=>{t+=c.price}),t.toFixed(2)}function T(){e&&e.length!==0?a.innerHTML=h():a.innerHTML=l()}T();const H=a.querySelector(".cart_products_list");_();function h(){const t=f();return`<div class="products_container">
+import{s as r,u as m,i as g,f as L}from"./assets/icons-03779629.js";import"./assets/vendor-99d50140.js";const u="/project-code_busters09/assets/yellow_basket_desctop_1x-min-6400acde.png",$="/project-code_busters09/assets/yellow_basket_desctop_1x-min-6400acde.png",w="/project-code_busters09/assets/yellow_basket_mobile_1x-min-195418a1.png",M="/project-code_busters09/assets/yellow_basket_desctop_2x-min-91220a6c.png",S="/project-code_busters09/assets/yellow_basket_desctop_2x-min-91220a6c.png",T="/project-code_busters09/assets/yellow_basket_mobile_2x-min-566aede4.png",b=document.querySelector(".modal-cart-submit"),E=document.querySelector(".cart-modal-content"),o=document.getElementById("cart-content");o.addEventListener("click",q);document.querySelector(".cart_products_container");const v=document.querySelector(".cart-quentity");let e=r.getFromStorage("cart");function q(t){t.target.closest(".cart-delete-all-button")&&(r.clearCart(),m(),v.textContent="Cart(0)",o.innerHTML=l())}function f(){if(e){const t=e.length;v.textContent=`Cart (${t})`}}f();function y(){let t=0;return e&&e.forEach(s=>{t+=s.price}),t.toFixed(2)}function j(){e&&e.length!==0?o.innerHTML=_():o.innerHTML=l()}j();const H=o.querySelector(".cart_products_list");h();function _(){const t=y();return`<div class="products_container">
   <div class="cart-add-products">      
     <div class="delete-all">        
       <button type="submit" class="cart-delete-all-button">Delete all            
         <svg class="cart_close_all" width="24" height="24">
-          <use xlink:href="${u}#icon-ion_close-sharp"></use>
+          <use xlink:href="${g}#icon-ion_close-sharp"></use>
         </svg>            
       </button>          
     </div>        
@@ -38,32 +38,15 @@ import{s as r,u as g,i as u,f as L}from"./assets/icons-d533626e.js";import"./ass
         </label>
       </div>
       <div class="cart-btn">
-        <button type="submit" id="checkoutButton" class="cart-checkout-button">Checkout</button>
+        <button type="submit" class="cart-checkout-button">Checkout</button>
       </div>
     </form>
-    
-    <div id="modal-cart" class="modal-cart-del" style="display: none;">
-    <div class="modal-overlay">
-      <div class="cart-modal-content">   
-      <button class="cart-delete-modal">
-          <svg class="cart_close_all" width="18" height="18">
-            <use xlink:href="${u}#icon-ion_close-sharp"></use>
-          </svg>
-        </button>
-
-        <img class="cart-modal-img" src="${p}" alt="basket" />
-        <div>
-        <p class="cart-success">Order success</p>
-        <p class="cart-modal-info">Thank you for shopping at Food Boutique. Your order has been received and is now being freshly prepared just for you! Get ready to indulge in nourishing goodness, delivered right to your doorstep. We're thrilled to be part of your journey to better health and happiness</p>
-      </div>
-      </div>
-  </div>
-  </div>`}function _(){if(e){const t=e.map(({_id:s,name:i,img:n,category:o,size:d,price:C})=>`      
-    <li id="${s}" class="cart-list">
+    `}function h(){if(e){const t=e.map(({_id:c,name:i,img:n,category:a,size:d,price:C})=>`      
+    <li id="${c}" class="cart-list">
       <div class="obj-delete">
         <button class="cart-delete-button">
           <svg class="cart_close_all" width="18" height="18">
-            <use xlink:href="${u}#icon-ion_close-sharp"></use>
+            <use xlink:href="${g}#icon-ion_close-sharp"></use>
           </svg>
         </button>
       </div>
@@ -74,17 +57,17 @@ import{s as r,u as g,i as u,f as L}from"./assets/icons-d533626e.js";import"./ass
         <div class="add-img-info">
           <p class="product-name">${i}</p>
           <div class="product-category-size">
-            <p class="product-category">Category: <span class="colored-text">${o}</span></p>
+            <p class="product-category">Category: <span class="colored-text">${a}</span></p>
             <p class="product-size">Size: <span class="colored-text">${d}</span></p>
           </div>
           <p class="product-price">$ ${C}</p>
         </div>
       </div>
-    </li>`).join("");if(e.length===0)return;H.innerHTML=t,document.querySelectorAll(".cart-delete-button").forEach(s=>s.addEventListener("click",F))}}function F(t){const c=t.target.closest("li").id;r.removeFromCart(c),g(),e=r.getFromStorage("cart"),e.length===0&&(a.innerHTML=l());const s=document.querySelector(".cart-sum-number");s&&(s.textContent=f()),h(),_(),y()}function l(){return`  
+    </li>`).join("");if(e.length===0)return;H.innerHTML=t,document.querySelectorAll(".cart-delete-button").forEach(c=>c.addEventListener("click",U))}}function U(t){const s=t.target.closest("li").id;r.removeFromCart(s),m(),e=r.getFromStorage("cart"),e.length===0&&(o.innerHTML=l());const c=document.querySelector(".cart-sum-number");c&&(c.textContent=y()),_(),h(),f()}function l(){return`  
   <div class="box-img">
     <picture>
       <source media="(min-width: 1440px)" srcset="
-          ${p} 1x,
+          ${u} 1x,
           ${M} 2x
         " type="image/png" />
       <source media="(min-width: 768px)" srcset="
@@ -93,9 +76,9 @@ import{s as r,u as g,i as u,f as L}from"./assets/icons-d533626e.js";import"./ass
         " type="image/png" />
       <source media="(min-width: 320px)" srcset="
           ${w} 1x,
-          ${q} 2x
+          ${T} 2x
         " type="image/png" />
-      <img class="cart-img" src="${p}" alt="basket" />
+      <img class="cart-img" src="${u}" alt="basket" />
     </picture>
   </div>
   <div class="cart-empty">
@@ -107,9 +90,15 @@ import{s as r,u as g,i as u,f as L}from"./assets/icons-d533626e.js";import"./ass
       the cart.
     </p>
   </div>
-</div>`}if(document.querySelector(".order-form")){let c=function(s){s.preventDefault();const i=r.getCart().map(o=>({productId:o._id,amount:1})),n=document.querySelector(".cart-email");n.value&&L.order(n.value,i).then(o=>{console.log(o.data.message);const d=o.data.message;r.clearCart(),g(),v.textContent="Cart(0)",a.innerHTML=l(),E.innerHTML=R(d),U()}).catch(o=>{console.log(o)}).finally()};document.querySelector(".order-form").addEventListener("submit",c)}function U(){b.style.display="block",document.body.classList.add("no-scroll"),document.addEventListener("keydown",k),window.addEventListener("click",x)}function m(){b.style.display="none",document.removeEventListener("keydown",k),window.removeEventListener("click",x),document.body.classList.remove("no-scroll")}function k(t){t.key==="Escape"&&m()}function x(t){(t.target.classList.contains("modal")||t.target.closest(".modal-btn-close"))&&m()}function R(t){return`
-  <button class="modal-btn-close">X
-</button>
-<p>${t}</p>
-`}
+</div>`}if(document.querySelector(".order-form")){let s=function(c){c.preventDefault();const i=r.getCart().map(a=>({productId:a._id,amount:1})),n=document.querySelector(".cart-email");n.value&&L.order(n.value,i).then(a=>{console.log(a.data.message);const d=a.data.message;r.clearCart(),m(),v.textContent="Cart(0)",o.innerHTML=l(),E.innerHTML=R(d),F()}).catch(a=>{console.log(a)}).finally()};document.querySelector(".order-form").addEventListener("submit",s)}function F(){b.style.display="block",document.body.classList.add("no-scroll"),document.addEventListener("keydown",k),window.addEventListener("click",x)}function p(){b.style.display="none",document.removeEventListener("keydown",k),window.removeEventListener("click",x),document.body.classList.remove("no-scroll")}function k(t){t.key==="Escape"&&p()}function x(t){(t.target.classList.contains("cart-modal-content")||t.target.closest(".cart-delete-modal"))&&p()}function R(t){let s=t.substring(0,t.indexOf("Thank you for shopping")),c=t.substring(t.indexOf("Thank you for shopping"));return s=s.toUpperCase().replace("!",""),`
+  <button class="cart-delete-modal">
+    <svg class="cart_close_all" width="18" height="18">
+      <use xlink:href="${g}#icon-ion_close-sharp"></use>
+    </svg>
+  </button>
+  <img class="cart-modal-img" src="${u}" alt="basket" />
+  
+  <h1 class="cart-success">${s}</h1>
+  <p class="cart-modal-info">${c}</p>
+  `}
 //# sourceMappingURL=commonHelpers.js.map
